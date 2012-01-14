1ab11d04a9f0615a513e4dff17df6ecd039cb3f7
osakana4242 <osakana4242@gmail.com>
2012-01-14 14:33:06 +0900

    初回コミット

---
+++ project/itunes_furikake/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("iTunesふりかけ")]
+[assembly: AssemblyDescription("iTunesで選択されているトラックに読みを振る")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("iTunesふりかけ")]
+[assembly: AssemblyCopyright("三途の川おさかな")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("ad1d40ac-8b36-43b8-8218-4d194ccfd777")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja-JP")]
---
+++ project/itunes_furikake/RootForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using iTunesLib;
+
+namespace App
+{
+    public partial class RootForm : Form
+    {
+        private RubyAdder rubyAdder;
+        public RootForm(RubyAdder rubyAdder)
+        {
+            InitializeComponent();
+            this.rubyAdder = rubyAdder;
+            this.rubyAdder.setLogger(this.addLog);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // ボタン：ひらがな.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.exec(RubyAdder.OPE.HIRAGANA);
+        }
+
+        // ボタン：アルファベット.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.exec(RubyAdder.OPE.ALPHABET);
+        }
+
+        // ボタン：クリア.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.exec(RubyAdder.OPE.CLEAR);
+        }
+
+        // ボタン：バージョン情報の表示.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            VersionForm versionForm = new VersionForm();
+            versionForm.ShowDialog(this);
+            versionForm.Dispose();
+        }
+
+        private void exec(RubyAdder.OPE ope)
+        {
+            console.Clear();
+            this.Enabled = false;
+            ProgressDialog progressDialog = new ProgressDialog(this, this.rubyAdder.makeDoWorkEventHandler(), ope);
+            progressDialog.Text = App.Properties.Resources.StrExecuting;
+            progressDialog.Show(this);
+//            this.rubyAdder.Exec(ope);
+        }
+
+        // 強制書き換えチェックボックス。
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.rubyAdder.IsForceAdd = this.checkBox1.Checked;
+        }
+
+        // ユーザに見せるログを追加.
+        public void addLog(string str)
+        {
+            this.console.AppendText(str);
+        }
+
+    }
+
+
+}
